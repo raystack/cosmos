c0b7b59b07106840032de2107e87ed13100b8c73
Rishabh Mishra <me@rishabhmishra.dev>
2021-02-16 15:56:13 +0530

    chore: setup basic hapi server

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
