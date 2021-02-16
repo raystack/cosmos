33e4fede8caa69bb931965d1d72535b50a2c7b2a
Rishabh Mishra <me@rishabhmishra.dev>
2021-02-16 16:36:34 +0530

    feat: add swagger docs for api

--- src/app/health/handler.ts
+++ src/app/health/handler.ts
@@ -2,6 +2,7 @@ import Hapi from '@hapi/hapi'
 
 export const ping = {
   description: 'pong the request',
+  tags: ['api'],
   handler: (_req: Hapi.Request, _h: Hapi.ResponseToolkit) => {
     return {
       statusCode: 200,
