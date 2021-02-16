0c46101caa55b3e77a690068f41b3855db58302c
Rishabh Mishra <me@rishabhmishra.dev>
2021-02-17 01:15:31 +0530

    test: add test for health route

--- tests/app/health/hander.ts
+++ tests/app/health/hander.ts
@@ -1,8 +1,39 @@
-// import * as Config from 'src/config/config';
-// import HealthPlugin from 'src/app/health';
+import Hapi, { ServerInjectOptions } from '@hapi/hapi';
+import * as HealthPlugin from 'src/app/health';
+import * as Config from 'src/config/config';
 
-describe('Test', () => {
-  test('test', () => {
-    expect(true).toBe(true);
+let server: Hapi.Server;
+
+beforeAll(async () => {
+  const plugins: any = [HealthPlugin];
+  server = new Hapi.Server({
+    port: Config.get('/port/api'),
+    debug: false
+  });
+  await server.register(plugins);
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+describe('HealthCheck::Handler::ping', () => {
+  let request: ServerInjectOptions;
+  beforeEach(() => {
+    request = {
+      method: 'GET',
+      url: '/ping'
+    };
+  });
+  test('returns :ok status', async () => {
+    const expectedResult = {
+      statusCode: 200,
+      status: 'ok',
+      message: 'pong'
+    };
+
+    const response = await server.inject(request);
+    expect(response.statusCode).toBe(200);
+    expect(response.result).toEqual(expectedResult);
   });
 });
