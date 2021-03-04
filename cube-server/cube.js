147164614d84d334b861f3adc9e0429ed172d27e
Rishabh Mishra <me@rishabhmishra.dev>
2021-03-04 11:06:31 +0530

    feat: add custom driverFactory to cube server

--- cube-server/cube.js
+++ cube-server/cube.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable @typescript-eslint/no-var-requires */
 // Cube.js configuration options: https://cube.dev/docs/config
 const fetch = require('node-fetch');
+const PostgresDriver = require('@cubejs-backend/postgres-driver');
 
 const COSMOS_URL = process.env.COSMOS_URL || 'http://localhost:8000';
 
@@ -22,6 +24,24 @@ class CosmosSchemaRepository {
   }
 }
 
+async function getConnection(urn) {
+  return fetch(`${COSMOS_URL}/api/connections/${urn}`)
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+}
+
+async function driverFactory(driverContext) {
+  const { dataSource } = driverContext;
+  const { data: connection } =
+    dataSource !== 'default' ? await getConnection(dataSource) : {};
+  if (connection.type === 'postgres') {
+    return new PostgresDriver({ ...connection.credentials, readOnly: true });
+  }
+  return null;
+}
+
 module.exports = {
-  repositoryFactory: () => new CosmosSchemaRepository()
+  dbType: 'postgres', // TODO: need to call connection api to get
+  repositoryFactory: () => new CosmosSchemaRepository(),
+  driverFactory
 };
