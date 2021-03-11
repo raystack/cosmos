8ffb8464f22ddde243521474fafd3b406082531d
Rishabh Mishra <me@rishabhmishra.dev>
2021-03-11 17:42:30 +0530

    feat: add create metric handler

---
+++ src/app/metric/handler.ts
@@ -0,0 +1,22 @@
+import Hapi from '@hapi/hapi';
+import { ICreateMetricPayload } from 'src/types';
+import * as Resource from './resource';
+import * as Schema from './schema';
+
+export const create = {
+  description: 'Create Metric',
+  tags: ['api'],
+  validate: {
+    payload: Schema.createPayload
+  },
+  response: {
+    status: {
+      200: Schema.createResponse
+    }
+  },
+  handler: async (req: Hapi.Request, _h: Hapi.ResponseToolkit) => {
+    const metric = await Resource.create(<ICreateMetricPayload>req.payload);
+    console.log(metric);
+    return { data: metric };
+  }
+};
