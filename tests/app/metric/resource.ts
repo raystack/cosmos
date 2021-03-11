9cbc37908817a3b8cccd77cf19d30c87bc2bd6be
Rishabh Mishra <me@rishabhmishra.dev>
2021-03-11 22:03:36 +0530

    tests: add tests for metrics list api

--- tests/app/metric/resource.ts
+++ tests/app/metric/resource.ts
@@ -2,7 +2,7 @@ import Metric from 'src/models/metric';
 import * as Resource from 'src/app/metric/resource';
 import * as Transformer from 'src/app/metric/transformer';
 
-import { ICreateMetricPayload } from 'src/types';
+import { ICreateMetricPayload, IMetricDocument } from 'src/types';
 
 afterEach(() => {
   jest.resetAllMocks();
@@ -63,6 +63,27 @@ describe('Connection::Metric', () => {
       const result = await Resource.get(urn);
       expect(result).toBeNull();
       expect(spy).toHaveBeenCalledWith(urn);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('list', () => {
+    test('should return empty list', async () => {
+      const spy = jest.spyOn(Metric, 'list').mockResolvedValueOnce([]);
+      const expectedResult: IMetricDocument[] = [];
+      const result = await Resource.list();
+      expect(result).toEqual(expectedResult);
+      expect(spy).toHaveBeenCalledWith(/* empty */);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    test('should return metrics list', async () => {
+      const metrics = [new Metric({ urn: 1 }), new Metric({ urn: 2 })];
+      const spy = jest.spyOn(Metric, 'list').mockResolvedValueOnce(metrics);
+      const expectedResult: IMetricDocument[] = metrics;
+      const result = await Resource.list();
+      expect(result).toEqual(expectedResult);
+      expect(spy).toHaveBeenCalledWith(/* empty */);
       expect(spy).toHaveBeenCalledTimes(1);
     });
   });
