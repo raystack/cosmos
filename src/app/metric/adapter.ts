86dfcb419a5997b7d088f5480630c47b45c280e6
Rishabh Mishra <me@rishabhmishra.dev>
2021-03-17 09:30:13 +0530

    Revert "feat: add cube provider to fetch sql"
    This reverts commit 7a4a651ea7ad9ec438485619603ae11a394d0f93.

--- src/app/metric/adapter.ts
+++
@@ -1,8 +0,0 @@
-import * as CubeProvider from 'src/providers/cube';
-import { IMetric, IMetricResponse } from 'src/types';
-
-export async function addSql(metric: IMetric): Promise<IMetricResponse> {
-  const { fields } = metric;
-  const sql = await CubeProvider.getSql(fields);
-  return { ...metric, sql };
-}
